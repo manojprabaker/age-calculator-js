97010d35bb5e9a111cb4d65de0a739498feebe8f
MJ <manojprabaker999@gmail.com>
2026-06-06 19:22:30 +0530

    test commit for git sync

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ btn.addEventListener("click", function () {
   let dd = dateToday.getDate();
   let mm = dateToday.getMonth() + 1;
   let yy = dateToday.getFullYear();   
-    
+
   
   //console.log(dd,mm,yy);
      
@@ -28,7 +28,7 @@ btn.addEventListener("click", function () {
     yy --;
   }
   let dat = dd - d;
-  let mon = mm - m;
+  let mon = mm - m; 
   let year = yy - y;
   
   res.innerHTML=`${year} years ${mon} months  ${dat} days`
