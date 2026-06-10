aeca2d0becc9483981e84c5c3e5c145bd3046f1c
MJ <manojprabaker999@gmail.com>
2026-06-10 18:15:14 +0530

    minor changes

--- main.js
+++ main.js
@@ -10,27 +10,28 @@ btn.addEventListener("click", function () {
   let m = parseInt(document.getElementById("month").value);
   let y = parseInt(document.getElementById("year").value);
 
+
   let dateToday = new Date();
   let dd = dateToday.getDate();
   let mm = dateToday.getMonth() + 1;
-  let yy = dateToday.getFullYear();   
+  let yy = dateToday.getFullYear();
 
-  
+
   //console.log(dd,mm,yy);
-     
+
   let month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
   if (d > dd) {
     dd = dd + month[mm - 1];
-    mm --;
+    mm--;
   }
   if (m > mm) {
     mm = mm + 12;
-    yy --;
+    yy--;
   }
   let dat = dd - d;
-  let mon = mm - m; 
-  let year = yy - y ;
-  
-  res.innerHTML=`${year} years ${mon} months  ${dat} days old`;
-  console.log(dat,mon,year);
+  let mon = mm - m;
+  let year = yy - y;
+
+  res.innerHTML = `${year} years ${mon} months  ${dat} days old`;
+  console.log(dat, mon, year);
 });
